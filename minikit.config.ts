9b7ff092667673e46577995ba2189383ff9ef09d
CodeByNikolas <Nikolas.Hack@tum.de>
2025-10-20 17:38:42 +0200

    bob rocks

--- minikit.config.ts
+++ minikit.config.ts
@@ -8,12 +8,12 @@ const ROOT_URL =
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
 export const minikitConfig = {
-  "accountAssociation": {
+  accountAssociation: {
     header: "eyJmaWQiOjEzOTQ1NTksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyZUYwMjdjZjg0RDc1MjM1NTY2YjEwNzYzMWE2QkY3OEM4ODAxQTQ1In0",
     payload: "eyJkb21haW4iOiJiYXNlLWhhY2thdGhvbi1iYXNlZC1iaWxscy52ZXJjZWwuYXBwIn0",
     signature: "2p0gPbn0t2/J+Iuyz3KRcEmzapxoA+cCMLi0HV+et5c/BrzTMt4ixNvFnKjbLKva8L5882PDqZXO0rrAHRIV6xw="
   },
-  "frame": {
+  frame: {
     version: "1",
     name: "Example Frame",
     iconUrl: "https://base-hackathon-based-bills.vercel.app/icon.png",
@@ -23,6 +23,9 @@ export const minikitConfig = {
     splashImageUrl: "https://base-hackathon-based-bills.vercel.app/splash.png",
     splashBackgroundColor: "#eeccff",
     webhookUrl: "https://base-hackathon-based-bills.vercel.app/api/webhook"
+  },
+  baseBuilder: {
+    ownerAddress: "0x23F5d0A18DafB06247F47a9C41FF39F483CFe060"
   }
 } as const;
 
